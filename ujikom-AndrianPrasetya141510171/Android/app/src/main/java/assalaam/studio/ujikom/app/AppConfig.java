6fd6ddd051e98d1f23a5960d85ef0dd9589bf07f
smk-141510171 <141510171@smkassalaambandung.sch.id>
2017-02-25 08:59:28 +0700

    Android Project

---
+++ ujikom-AndrianPrasetya141510171/Android/app/src/main/java/assalaam/studio/ujikom/app/AppConfig.java
@@ -0,0 +1,9 @@
+package assalaam.studio.ujikom.app;
+
+public class AppConfig {
+	// Server user login url4
+	public static String URL_LOGIN = "http://192.100.100.78/PenggajianAndrian/public/api/login";
+
+	// Server user register url
+	public static String URL_REGISTER = "http://ujikom-2017.smkassalaambandung.sch.id/api/register";
+}
